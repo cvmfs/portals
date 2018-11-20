6aa9ed6f3d29e83371337de440954475349b8c80
Simone Mosciatti <simone.mosciatti@cern.ch>
2018-11-20 10:51:55 +0100

    remove commented out imports

--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	/*
-		"fmt"
-		"os"
-	*/
-
 	"github.com/siscia/portals/cmd"
 	_ "github.com/siscia/portals/cvmfs"
 	_ "github.com/siscia/portals/lib"
-	/*
-		"github.com/aws/aws-sdk-go/aws"
-		"github.com/aws/aws-sdk-go/aws/session"
-		"github.com/aws/aws-sdk-go/service/s3"
-		"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	*/)
+)
 
 func main() {
 	cmd.EntryPoint()
