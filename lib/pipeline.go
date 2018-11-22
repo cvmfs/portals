47a60f1c6fe8203f1163a730eda93427b94b9778
Simone Mosciatti <simone.mosciatti@cern.ch>
2018-11-22 16:06:08 +0100

    work on implementation of the interface

--- lib/pipeline.go
+++ lib/pipeline.go
@@ -1,11 +1,20 @@
 package lib
 
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
 /*
 The idea here is to have a pipeline, where:
 1. Element enter
 2. They are downloaded into local storange
 3. They are ingested into CVMFS
 4. Local resource are cleaned up
+5. Element exits
 
 What happen in case of failure?  We can return a structure that implement the
 interface of the next pipeline, on this structure we can simply shortcut all
@@ -81,14 +90,34 @@ func NewPipeline() (chan<- PipelineInput, <-chan PipelineOutput) {
 
 type GenericError struct{}
 
-func (e GenericError) DownloadFile() S3LocalFile {
+func (e GenericError) DownloadFile() IS3LocalFile {
 	return GenericError{}
 }
 
-func (e GenericError) Ingest() S3IngestedFile {
+func (e GenericError) Ingest() IS3IngestedFile {
 	return GenericError{}
 }
 
 func (e GenericError) Cleanup() PipelineOutput {
 	return PipelineOutput{}
+}
+
+type S3Object struct {
+	Key     string
+	Hash    string
+	Session *session.Session
+}
+
+func NewS3Object(s3obj s3.Object, session *session.Session) S3Object {
+	toHash := []byte(fmt.Sprintf("%s%d", *s3obj.Key, s3obj.LastModified.Unix()))
+	hash := fmt.Sprintf("%s", sha256.Sum256(toHash))
+	return S3Object{Key: *s3obj.Key, Hash: hash, Session: session}
+}
+
+func (s3obj S3Object) MakeS3RemoteFile() IS3RemoteFile {
+	return s3obj
+}
+
+func (s3obj S3Object) DownloadFile() IS3LocalFile {
+	return s3obj
 }
